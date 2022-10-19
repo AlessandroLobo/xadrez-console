44426af7265ee6a6d5e4fad0120c2875eb5e191a
Alessandro Lobo <alessandro.lobo@hotmail.com>
2022-10-19 18:53:42 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- Program.cs
+++ Program.cs
@@ -6,9 +6,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tabuleiro tab = new Tabuleiro(10, 8);
 
-            
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
 
         }
     }
